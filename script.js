02943da2b1a3e2b71c484d894dc59440ef1d1e44
gugansuresh26-a11y <gugansuresh26@gmail.com>
2025-09-10 22:24:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    document.getElementById("formStatus").textContent = "Thank you! Your message has been sent.";
+    this.reset();
+});
